218d55bd20d6eb22945caf2d813b40145455fbe7
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-06 06:14:55 +0000

    type casting

---
+++ python-lessons/type-casting.py
@@ -0,0 +1,13 @@
+# length = len(input("What is your name"))
+# print("Your name has " + str(length) + " characters")
+
+# print(type(length))
+
+print("Enter two numbers to add:")
+
+num_1 = input("enter first number")
+num_2 = input("enter second number")
+
+sum = int(num_1) + int(num_2)
+
+print(sum)
